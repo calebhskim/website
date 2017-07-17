b7ea6be2f4382a4ebdd49980b904425c4582af8a
calebhskim <caleb.hs.kim@gmail.com>
2017-07-17 00:31:16 -0700

    Update dependencies. Add grommet

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {
-  BrowserRouter,
-  Link,
-  Route,
-  StaticRouter,
-} from 'react-router-dom';
 
 import Home from './Home';
 
-const App = (props) => {
-  const Router = props.isBuild ? StaticRouter : BrowserRouter;
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-        </ul>
-        <Route path='/' component={Home} exact={true} />
-      </div>
-    </Router>
-  );
-};
+const App = () => (<Home />);
 
-App.propTypes = {
-  isBuild: PropTypes.bool,
-};
-
-App.defaultProps = {
-  isBuild: false,
-};
+App.defaultProps = {};
 
 export default App;
